dcf09dd1654eb0e87d1a669ff09194b8d603d298
Altimann <kev-web@users.noreply.github.com>
2022-02-22 20:10:28 -0600

    static urls.py

--- petstore/urls.py
+++ petstore/urls.py
@@ -20,13 +20,13 @@ from django.urls import path, include
 #By default, Django includes the app "django.contrib.staticfiles", which serves your static files from each app (no need for these imports), but
 #if you wanted to 'serve' media files (uploaded images/files by users), you need to use these imports, and bottom code (media part only).
 #If we where to DELETE "django.contrib.staticfiles" app in settings.py, we NEED these 2 imports in order to serve media & static files.
-# from django.conf import settings
-# from django.conf.urls.static import static
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin-edu-9005!/', admin.site.urls),
     path('', include('simbapp.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 #We add this if we want to serve media/static in development.
 #If 'django.contrib.staticfiles' app is NOT deleted in settings.py, then we only need to add the 'media' part.
