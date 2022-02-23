b3a70f20308325cc74b0111aea3a544926d3976c
Altimann <kev-web@users.noreply.github.com>
2022-02-22 20:52:46 -0600

    again bro bor  broooo

--- petstore/settings.py
+++ petstore/settings.py
@@ -162,10 +162,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
-
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
